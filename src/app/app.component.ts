b26c73d676156741c3b8f5904e7d8923f0408fde
Nick Hultz <nhultz328@gmail.com>
2018-09-30 10:32:59 -0400

    Add firebase deployment to cloudbuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Learning Centers';
+  title: string;
+
+  constructor() {
+    if (environment.name === 'prod') {
+      this.title = 'Learning Centers';
+    } else {
+      this.title = `Learning Centers (${environment.name})`;
+    }
+  }
 }
