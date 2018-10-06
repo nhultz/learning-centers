85e5a074213d6bf769c7f16c5dc68b05a642ab3c
Nick Hultz <nhultz328@gmail.com>
2018-10-06 10:33:48 -0400

    Environment Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   name: 'prod',
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBYWq8rOpg0fLBNowpne3jz4L49qSO6rLg',
+    authDomain: 'learning-centers.firebaseapp.com',
+    databaseURL: 'https://learning-centers.firebaseio.com',
+    projectId: 'learning-centers',
+    storageBucket: 'learning-centers.appspot.com',
+    messagingSenderId: '866535837336'
+  }
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,12 @@
 export const environment = {
   name: 'staging',
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB6XoUSdIehKD5ZOwUWAt6VffQ7OX4k360',
+    authDomain: 'learning-centers-staging.firebaseapp.com',
+    databaseURL: 'https://learning-centers-staging.firebaseio.com',
+    projectId: 'learning-centers-staging',
+    storageBucket: 'learning-centers-staging.appspot.com',
+    messagingSenderId: '978920677579'
+  }
 };
